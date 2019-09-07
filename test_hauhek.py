99a6fe6eeb60b0042870578210c9bd9029c0ae1d
muyun <wenlzhao@gmail.com>
2019-09-07 13:51:02 +0800

    change the name

---
+++ test_hauhek.py
@@ -0,0 +1,22 @@
+#import pytest
+import unittest
+import time
+
+#TODO: how to call argparse
+
+from ..hauhek import segmentation
+
+class CansegTestCase(unittest.TestCase):
+    def setUp(self):
+        self.query = '呢幾日天氣成日變，你要小心保重身體'
+
+    def tearDown(self):
+        time.sleep()
+
+    def test_hauhek(sent):
+        output = segmentation()
+        print(output)
+        assert true
+
+if __name__ == "__main__":
+    unittest.main()
